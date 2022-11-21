77d756a1521b80a4112010982339ac3317a09692
Diego Alves <diegoteialves@gmail.com>
2022-11-21 20:39:21 -0300

    Continuação do menu

--- empresa.js
+++ empresa.js
@@ -4,26 +4,17 @@ class Cliente{
     #DataNascimento
 
     constructor(nome, cpf, dataNascimento){
+
         this.setNome(nome)
         this.setCpf(cpf)
         this.setDataNascimento(dataNascimento)
     }
 
     setNome(nome){
-        while(nome.length>50){
-            alert("Informe novamente seu nome, respeitando o número de caracteres máximo de 50.")
-            nome = prompt("Informe o nome novamente.")
-        }
-
         this.#Nome = nome
     }
 
     setCpf(cpf){
-        while(cpf.length>11 && cpf.length<11){
-            alert("Informe novamente seu cpf, respeitando o número de caracteres de 11.")
-            cpf = prompt("Informe o cpf novamente.")
-        }
-
         this.#Cpf = cpf
     }
 
@@ -255,16 +246,34 @@ let cliente
 let clientes = []
 let voo
 let voos = []
+let passagemArea
+let passagensAreas = []
+
 do {
     condicao = prompt("O que o deseja?" + "\n" + "1 = Criar novo cliente." + "\n" + "2 = Criar novo voo.")
     switch (condicao) {
         case "1":
             let nome = prompt("Informa o seu Nome.")
+            
+                while(nome.length>50){
+                    alert("Informe novamente seu nome, respeitando o número de caracteres máximo de 50.")
+                    nome = prompt("Informe o nome novamente.")
+                }
+
             let cpf = prompt("Informa o número do cpf")
+
+                while(cpf.length != 11){
+                    alert("Informe novamente seu cpf, respeitando o número de caracteres de 11.")
+                    cpf = prompt("Informe o cpf novamente.")
+                }
+
             let dataNascimento = prompt("Informe sua data de nascimento")
+
             cliente = new Cliente(nome, cpf, dataNascimento)
+
             clientes.push(cliente)
             break;
+
         case "2":
             let empresa = prompt("Informe o nome da empresa do voo.")
             let numero = parseInt(prompt("Informe o número do voo."))
@@ -272,16 +281,76 @@ do {
             let horario = prompt("Informe o horario do voo.")
             let localPartida = prompt("Informe o local de partida do voo.")
             let localDestino = prompt("Informe o local de destino do voo.")
+
             voo = new Voo(empresa, numero, data, horario, localPartida, localDestino)
+
             voos.push(voo)
+            break;
+
+        case "3":
+            alert("Escolha o cliente que esta efetuando a compra da passagem.")
+
+                for (let index = 0; index < clientes.length; index++) {
+                    console.log(clientes[index])
+                }
+
+            let passageiro = prompt("Informe aqui o nome do cliente completo e sem erros")
+
+            let cont = 0
+
+                while(cont == 0){
+                    for (let index = 0; index < clientes.length; index++) {
+                    if(passageiro == clientes[index].Nome){
+                        alert("Cliente autorizado!")
+                        cont++
+                    }
+                    }
+                    
+                    if(cont == 0){
+                        alert("Cliente não encontrado, repita o nome.")
+                        passageiro = prompt("Informe aqui o nome do cliente completo e sem erros")
+                    }
+                    cont = 0
+                }
+
+            alert("Escolha agora no nosso quadro de voos, o voo que você deseja!")
+
+                for (let index = 0; index < voos.length; index++) {
+                    console.log(voos[index])
+                }
+
+            let voo = promp("Informe o número do voo que deseja marcar sua passagem.")
+
+                while(cont == 0){
+                    for (let index = 0; index < voos.length; index++) {
+                    if(voo == voos[index].Numero){
+                        alert("Voo encontrado!")
+                        cont++
+                    }
+                    }
+                    
+                    if(cont == 0){
+                        alert("Voo não encontrado, repita o número do voo.")
+                        voo = promp("Informe o número do voo que deseja marcar sua passagem.")
+                    }
+
+                    cont = 0
+                }
+
+            
+
+            let
+            passagemArea = new PassagemArea(14, true, 1500.00, passageiro, voo)
+            break;
+
         case "6":
             alert("Obrigado por usar os nossos serviços!")
+            break;
         default:
             break;
     }
 } while (condicao != "6");
 
-let passagemArea = new PassagemArea(14, true, 1500.00, cliente, voo)
 passagemArea.CalcularValor()
 
 let pacoteViagem = new PacoteViagem(passagemArea, "28/01/2022")
